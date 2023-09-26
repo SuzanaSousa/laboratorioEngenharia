62a1c85e5b4c72cd2254a8feae4e55f2946cf863
SuzanaSousa <suzsousa10@gmail.com>
2023-09-26 12:28:29 -0300

    update

--- SextaSemana/script2.js
+++ SextaSemana/script2.js
@@ -5,17 +5,20 @@ const calculadoraDiv = document.getElementById('calculadora');
 const calculadora = document.createElement('div');
 calculadora.classList.add('calculadora');
 
+
 // Cria os elementos da calculadora (botões, visor, etc.)
 const visor = document.createElement('input');
 visor.type = 'text';
 visor.classList.add('visor');
+visor.classList.add('visor-centro'); // Adicione a classe "visor-centro" ao visor
 calculadora.appendChild(visor);
 
 const botoes = [
-    '7', '8', '9', '/',
-    '4', '5', '6', '*',
-    '1', '2', '3', '-', 'AC',
-    '0', '.', '=', '+', '+/-'
+    'C',' +/-','%', '/',
+    '7', '8', '9', '*',
+    '4', '5', '6', '-',
+    '1', '2', '3', '+',
+     '0', '.', '=','←',
 ];
 
 botoes.forEach(botao => {
@@ -25,16 +28,29 @@ botoes.forEach(botao => {
         // Lógica para tratar os cliques nos botões
         if (botao === '=') {
             // Realize o cálculo e exiba o resultado no visor
-            visor.value = eval(visor.value);
-        } else {
+           visor.value = eval(visor.value);
+        } else if (botao === 'C') {
+            // Limpe o visor
+            visor.value = '';
+        
+        }else if (botao === '+/-') {
+            // Inverter o sinal do número no visor
+            const valorAtual = parseFloat(visor.value) * -1;
+            visor.value = -valorAtual;
+        
+        }else if (botao === '%') {
+            // Calcular a porcentagem (dividir o valor atual por 100)
+            visor.value = eval(visor.value + '/ 100');
+        } else if (botao === '←') {
+            // Remove o último caractere do visor
+            visor.value = visor.value.slice(0, -1);
+        }else {
             // Adicione o valor do botão ao visor
-            visor.value += botao;
+            visor.value += botao;  
         }
     });
     calculadora.appendChild(btn);
 });
-
-
 
 // Adiciona a calculadora à div principal
 calculadoraDiv.appendChild(calculadora);
